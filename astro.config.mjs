bd6fbe6cad618edd417e3a3be452776c959cf5f1
tedd <15193400+epaumier@users.noreply.github.com>
2025-04-24 10:31:35 +0200

    removing ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
   i18n: {
     defaultLocale: 'fr',
     locales: ['fr', 'en'],
